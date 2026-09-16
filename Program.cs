79695df654601a96f6a951973342f6b9a72726dd
Filip Bergander <filip.bergander1996@gmail.com>
2026-09-16 16:02:50 +0200

    Lagt till data-filerna till projektet

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
-﻿using static System.Console;
+﻿using Microsoft.ML;
+using static System.Console;
 
-namespace Basics
+namespace predictTitanicSurvivorRate
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            string csvPath = "titanic_small.csv";
+            //string csvPath = "Titanic-Dataset.csv";
+            MLContext mlContext = new MLContext(seed: 1);
+            if (!File.Exists(csvPath))
+            {
+                WriteLine($"Kunde inte hitta data-filen: {Path.GetFullPath(csvPath)}");
+                return;
+            }
+            else
+            {
+                WriteLine("Hittade filen!");
+            }
         }
     }
 }
